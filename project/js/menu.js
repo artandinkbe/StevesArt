02ccf1e69d2f56981f70c880604def3fc7cc6d88
artandinkbe <125823006+artandinkbe@users.noreply.github.com>
2023-02-25 00:05:46 +0100

    Add files via upload

---
+++ project/js/menu.js
@@ -0,0 +1,14 @@
+	function openNav() {
+	  document.getElementById("mySidenav").style.width = "70%";
+	  
+	  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+	  document.getElementById("rmenudiv").style.display = "none";
+	}
+
+	function closeNav() {
+	  document.getElementById("mySidenav").style.width = "0";
+	 
+	  document.body.style.backgroundColor = "black";
+	  document.getElementById("rmenudiv").style.display = "block";
+	}
+	
